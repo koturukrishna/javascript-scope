18d03b7618b5cb7378af1b61772b88810ac4b637
krishna <krishnakoturu526@gmail.com>
2025-01-20 11:26:40 +0530

    adding practice files

---
+++ practice1.js
@@ -0,0 +1,14 @@
+// Create variables with your info
+let yourName = "Krishna Koturu";
+let yourAge = 30;
+let isStudent = true;
+
+// Try to change const
+const graduationYear = 2013;
+// graduationYear = 2024; // Will this work?
+
+// Print all variables
+console.log("Name:", yourName);
+console.log("Age:", yourAge);
+console.log("Student Status:", isStudent);
+console.log("Graduation Year:", graduationYear);
